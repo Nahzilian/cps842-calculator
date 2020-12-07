81c703f6d22b84bb48f9b73520900d862a0c379c
Doug <nahz3112@gmail.com>
2020-12-07 11:04:28 -0500

    Added new function for recommendation system

--- main.py
+++ main.py
@@ -1,18 +1,7 @@
 import numpy as np
-
+import math
 # Link analysis
  
-def matrix_calculator(vec1,vec2):
-    """
-    @Param
-    vec1: List, 1x3 vector
-    vec2: List, 3x3 vector
-    """
-    return np.dot(vec1,vec2)
-a = np.array([[0.03, 0.48, 0.48],[0.03, 0.03, 0.93],[0.03, 0.93, 0.93]])
-b = np.array([0.03,0.48,0.48])
-print(matrix_calculator(b,a))
-
 def page_rank_hit(arr,num,alpha):
     x_0 = np.array([1,0,0])
     arr = arr * (1 - alpha)
@@ -24,3 +13,55 @@ def page_rank_hit(arr,num,alpha):
 
 test = np.array([[0, 0.5, 0.5],[0, 0, 1],[0, 1, 0]])
 print(page_rank_hit(test,2,0.1))
+
+
+# Collaborative filtering (CF)
+def user_based_sim(arr):
+    """
+    Accept only 2 users per array
+    """
+    avg = [sum(arr[i])/arr.shape[1] for i in range(arr.shape[0])]
+    numarator = 0
+    a_set = []
+    b_set = []
+    for i in range(arr[0].shape[0]):
+        numarator += (arr[0][i] - avg[0]) * (arr[1][i] - avg[1])
+        a_set.append((arr[0][i] - avg[0]) ** 2)
+        b_set.append((arr[1][i] - avg[1]) ** 2)
+    return numarator/ math.sqrt(sum(a_set) * sum(b_set))
+print(user_based_sim(np.array([[2,3,4],[3,4,5]])))
+
+# np.array([
+# [2,3,4,5], book1
+# [3,4,4,2], book2
+# [5,0,2,2], book3
+# [4,5,0,4], book4
+# [0,5,3,5]]) book5
+
+def sim_calc(a,b, avg_a,avg_b):
+    numarator = 0
+    a_set = []
+    b_set = []
+    for i in range(a.shape[0]):
+        numarator += (a[i] - avg_a) * (b[i] - avg_b)
+        a_set.append((a[i] - avg_a) ** 2)
+        b_set.append((b[i] - avg_b) ** 2)
+    result = numarator/ math.sqrt(sum(a_set) * sum(b_set))
+    if not math.isnan(result):
+        return result
+    else:
+        return 0
+# This function should accept all, however, it will not give right result
+# due to the different in arrays (i.e If there's ranking for 1 item but not the other)
+
+def item_based(arr, r_item):
+    avg = [sum(arr[i])/arr.shape[1] for i in range(arr.shape[0])]
+    sim = []
+    for i in range(arr.shape[0]):
+        avg_a = avg[i]
+        avg_b = sum(r_item)/len(r_item)
+        sim.append( sim_calc(arr[i],r_item,avg_a,avg_b))
+    print(sim)
+
+item_based(np.array([[3,5]]),[2,2])
+
